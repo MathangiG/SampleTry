5ad863f0a65c353fbcb70278472427fc99dc64f5
MathangiG <gmathangibtech@gmail.com>
2021-08-11 09:18:38 +0530

    Sample

--- src/org/computer/Computer.java
+++ src/org/computer/Computer.java
@@ -4,5 +4,13 @@ public class Computer {
 	public void computerModel() {
 		System.out.println("Computer Model is DELL-JG54LMD");
 	}
+	
+	public void test1() {
+		
+	}
+	
+	public void test2() {
+
+	}
 
 }
